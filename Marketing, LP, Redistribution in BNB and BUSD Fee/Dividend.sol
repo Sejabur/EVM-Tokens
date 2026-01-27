ee36981cb8b90724c89795f47a162e140f9b95ed
Md Sejabur Rahat <74180183+Sejabur@users.noreply.github.com>
2026-01-27 15:09:02 +0600

    Taxed Token Source Codes

---
+++ Marketing, LP, Redistribution in BNB and BUSD Fee/Dividend.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC20.sol";
+import "./SafeMath.sol";
+import "./SafeMathInt.sol";
+import "./SafeMathUint.sol";
+
+interface IDividendPayingToken {
+
+  function dividendOf(address _owner) external view returns(uint256);
+
+  function distributeDividends() external payable;
+
+  function withdrawDividend() external;
+
+  event DividendsDistributed(
+    address indexed from,
+    uint256 weiAmount
+  );
+
+  event DividendWithdrawn(
+    address indexed to,
+    uint256 weiAmount
+  );
+}
+
+interface IDividendPayingTokenOptional {
+
+  function withdrawableDividendOf(address _owner) external view returns(uint256);
+  function withdrawnDividendOf(address _owner) external view returns(uint256);
+  function accumulativeDividendOf(address _owner) external view returns(uint256);
+}
+
+contract DividendPayingToken is ERC20, IDividendPayingToken, IDividendPayingTokenOptional {
+  using SafeMath for uint256;
+  using SafeMathUint for uint256;
+  using SafeMathInt for int256;
+
+  uint256 constant internal magnitude = 2**128;
+  uint256 internal magnifiedDividendPerShare;
+  uint256 internal lastAmount;
+  address public dividendToken;
+
+  mapping(address => int256) internal magnifiedDividendCorrections;
+  mapping(address => uint256) internal withdrawnDividends;
+
+  uint256 public totalDividendsDistributed;
+
+  constructor(string memory _name, string memory _symbol, address _token) ERC20(_name, _symbol) {
+    dividendToken = _token;
+  }
+
+  receive() external payable {
+  }
+
+  function distributeDividends() public override payable {
+    require(totalSupply() > 0);
+
+    if (msg.value > 0) {
+      magnifiedDividendPerShare = magnifiedDividendPerShare.add(
+        (msg.value).mul(magnitude) / totalSupply()
+      );
+      emit DividendsDistributed(msg.sender, msg.value);
+
+      totalDividendsDistributed = totalDividendsDistributed.add(msg.value);
+    }
+  }
+  
+
+  function distributeDividends(uint256 amount) public {
+    require(totalSupply() > 0);
+
+    if (amount > 0) {
+      magnifiedDividendPerShare = magnifiedDividendPerShare.add(
+        (amount).mul(magnitude) / totalSupply()
+      );
+      emit DividendsDistributed(msg.sender, amount);
+
+      totalDividendsDistributed = totalDividendsDistributed.add(amount);
+    }
+  }
+
+  function withdrawDividend() public virtual override {
+    _withdrawDividendOfUser(payable(msg.sender));
+  }
+  
+  function setDividendTokenAddress(address newToken) external virtual {
+     dividendToken = newToken;
+  }
+
+  function _withdrawDividendOfUser(address payable user) internal returns (uint256) {
+    uint256 _withdrawableDividend = withdrawableDividendOf(user);
+    if (_withdrawableDividend > 0) {
+      withdrawnDividends[user] = withdrawnDividends[user].add(_withdrawableDividend);
+      emit DividendWithdrawn(user, _withdrawableDividend);
+      bool success = IERC20(dividendToken).transfer(user, _withdrawableDividend);
+
+      if(!success) {
+        withdrawnDividends[user] = withdrawnDividends[user].sub(_withdrawableDividend);
+        return 0;
+      }
+
+      return _withdrawableDividend;
+    }
+
+    return 0;
+  }
+
+  function dividendOf(address _owner) public view override returns(uint256) {
+    return withdrawableDividendOf(_owner);
+  }
+
+  function withdrawableDividendOf(address _owner) public view override returns(uint256) {
+    return accumulativeDividendOf(_owner).sub(withdrawnDividends[_owner]);
+  }
+
+  function withdrawnDividendOf(address _owner) public view override returns(uint256) {
+    return withdrawnDividends[_owner];
+  }
+
+  function accumulativeDividendOf(address _owner) public view override returns(uint256) {
+    return magnifiedDividendPerShare.mul(balanceOf(_owner)).toInt256Safe()
+      .add(magnifiedDividendCorrections[_owner]).toUint256Safe() / magnitude;
+  }
+
+  function _transfer(address from, address to, uint256 value) internal virtual override {
+    require(false);
+
+    int256 _magCorrection = magnifiedDividendPerShare.mul(value).toInt256Safe();
+    magnifiedDividendCorrections[from] = magnifiedDividendCorrections[from].add(_magCorrection);
+    magnifiedDividendCorrections[to] = magnifiedDividendCorrections[to].sub(_magCorrection);
+  }
+
+  function _mint(address account, uint256 value) internal override { //Internal Function
+    super._mint(account, value);
+
+    magnifiedDividendCorrections[account] = magnifiedDividendCorrections[account]
+      .sub( (magnifiedDividendPerShare.mul(value)).toInt256Safe() );
+  }
+
+  function _burn(address account, uint256 value) internal override {
+    super._burn(account, value);
+
+    magnifiedDividendCorrections[account] = magnifiedDividendCorrections[account]
+      .add( (magnifiedDividendPerShare.mul(value)).toInt256Safe() );
+  }
+
+  function _setBalance(address account, uint256 newBalance) internal {
+    uint256 currentBalance = balanceOf(account);
+
+    if(newBalance > currentBalance) {
+      uint256 mintAmount = newBalance.sub(currentBalance);
+      _mint(account, mintAmount);
+    } else if(newBalance < currentBalance) {
+      uint256 burnAmount = currentBalance.sub(newBalance);
+      _burn(account, burnAmount);
+    }
+  }
+}
---
+++ Marketing, LP, Redistribution in BNB and BUSD Fee/Ownable.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./Context.sol";
+
+abstract contract Ownable is Context {
+    address private _owner;
+    address private _previousOwner;
+    uint256 private _lockTime;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+    //Locks the contract for owner for the amount of time provided
+    function lock(uint256 time) public virtual onlyOwner {
+        _previousOwner = _owner;
+        _owner = address(0);
+        _lockTime = block.timestamp + time;
+        emit OwnershipTransferred(_owner, address(0));
+    }
+}
